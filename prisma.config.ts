2e79959c5523aa1735aaf33acaf352e6ff83650d
Flow <talk2ibb2003@gmail.com>
2026-01-04 21:47:55 +0100

    .env and docker-compose.yml update

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  datasource: {
+  datasources: {
     db: {
-      url: process.env.DATABASE_URL,
+      url: process.env.DATABASE_URL!,
     },
   },
 });
